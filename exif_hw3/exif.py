7fbae612f9b23d3e4c564ae9c2b7955fe385fa88
kolovsam <samantha@pointed.com>
2015-03-04 14:25:57 -0500

    Add comments and documentation to exif program.
    Final touches to the exif homework. Made my output
    match the expected output for the homework. Also wrote
    docstrings for all the functions.

--- exif_hw3/exif.py
+++ exif_hw3/exif.py
@@ -14,20 +14,40 @@ from tags import TAGS
 
 class Exif(object):
     """
-    Parses EXIF tags from a JPEG file
+    Parses EXIF tags from a JPEG file.
+
+    Variables:
+        _filename (string): the name of the file to read in
+        _fd (file): the file being read in
+        _markers (int[]): array of marker locations in the file
+        _4d (int): location of the first M in the EXIF header
+        _num_entries (int): number of IFD entries
+        _entries_start (int): location of the first IFD entry 
+
+    Functions:
+        open_file()
+        verify_jpeg()
+        find_markers()
+        find_exif_header()
+        confirm_big_endian()
+        find_IFDstart()
+        print_entries()
+        print_value()
+
     """
+
     def __init__(self, filename):
         self._filename = filename
         self._fd = None
         self._markers = []
-        #self._sizes = []
         self._4d = 0
         self._num_entries = 0
         self._entries_start = 0
 
+
     def run(self):
         """
-        Run functions
+        Run functions.
         """
         self.open_file()
         self.verify_jpeg()  # part 1 (5pts)
@@ -37,22 +57,28 @@ class Exif(object):
         self.find_IFDstart() # part 4 (10pts)
         self.print_entries() # part 5 (20pts)
 
+
     def verify_jpeg(self):
         """
         Varifies that the file is JPEG by confirming that the first
         2 bytes of the file ar 0xffd8
+
+        Attributes:
+            jpeg (bytes): first two bytes of the file (should be 0xffd8)
+
         """
         # read first two bytes of the file
-        _jpeg = self._fd.read(2)
+        jpeg = self._fd.read(2)
 
         # should be 0xffd8...
-        if _jpeg == b"\xff\xd8":
+        if jpeg == b"\xff\xd8":
             print ("%s is a JPEG file." % self._filename)
         # else its not a jpeg file...
         else:
             print ("%s is NOT a JPEG file." % self._filename)
             print ("The program will exit.")
             sys.exit()
+
 
     def find_markers(self):
         """
@@ -64,6 +90,12 @@ class Exif(object):
         Each marker begins: 0xFFmmdddd, where mm is the marker number
         (ex. E0), and dddd is the length of the marker including the
         two bytes of 0xFFmm.
+
+        Attributes:
+            marker (bytes): 2 bytes of the marker
+            size (bytes): 2 bytes of the size
+            marker_str (str): printable representation of marker
+            size_str (str): printable representation of size
 
         """
 
@@ -81,14 +113,13 @@ class Exif(object):
             marker_str = ''.join('{:02X}'.format(b) for b in marker)
             size_str = ''.join('{:02X}'.format(b) for b in size)
             print ("[0x%04X] Marker 0x%s size=0x%s" %
-                    (self._fd.tell(), marker_str, size_str))
+                    (self._fd.tell()-4, marker_str, size_str))
 
             # add location of marker
             self._markers.append(self._fd.tell())
 
             # unpack size as a short
             size = unpack(">H", size)[0]
-            #self._sizes.append(size)
 
             # seek to next marker
             self._fd.seek(size-2, 1)
@@ -97,14 +128,20 @@ class Exif(object):
     def find_exif_header(self):
         """
         Locates the EXIF header.
+
+        Attributes:
+            exif (bytes): the bytes that may contain the exif header.
+
         """
 
         for marker in self._markers:
             self._fd.seek(marker)
             exif = self._fd.read(6)
 
+            # check if this marker contains the exif header
             if exif == b"Exif\x00\x00":
-                print ("Found an EXIF header!")
+                print ("Found the EXIF header at marker with location [0x%04X]!"
+                    % (marker-4))
                 return
 
         # didn't find exif header
@@ -116,6 +153,10 @@ class Exif(object):
         """
         Confirms Exif entries are big endian If it is little endian,
         the program will exit.
+
+        Attributes:
+            endian (bytes): the 4 bytes that tell you if the file is
+                            big or little endian.
         """
         self._4d = self._fd.tell()
 
@@ -129,10 +170,15 @@ class Exif(object):
         else:
             print ("This image is big endian!")
 
+
     def find_IFDstart(self):
         """
         In the marker with confirmed EXIF data, find the start of
         the IFD and print then number of entries.
+
+        Attributes:
+            ifd_offset (long): the offset to the start of the IFD entries.
+
         """
         # after confirming big endian, fd.tell points to the idf_offset
         ifd_offset = unpack(">L", self._fd.read(4))[0]
@@ -153,6 +199,19 @@ class Exif(object):
         Print name in hex and string equivalent for each entry if
         the format is 1, 2, 3, 4, 5, or 7. All other types will be
         ignored. 
+
+        Attributes:
+            tag (bytes): the tag for the entry
+            tag_str (str): the converted string for tag from the dictionary
+            form (short): format of the data for the entry
+            components (long): # of components for the entry
+            length (int): length in bytes of this tag's data
+            data (bytes): either the value or the offset for the entry
+
+        Note:
+            pylint does not like the 'end=" "' in the print statements. This prevents python
+            from creating a new line at each print statement...
+
         """
         bytes_per_component = (0,1,1,2,4,8,1,1,2,4,8,4,8)
 
@@ -195,6 +254,16 @@ class Exif(object):
     def print_value(self, form, components, length, data):
         """
         Print value of IFD entry according to format.
+
+        Args:
+            form (short): format of the data from the entry
+            components(long): # of components from the entry
+            length (int): length in bytes of the entry's tag's data
+            data (bytes): data to be printed for the entry
+
+        Prints:
+            The value of the data for the entry.
+
         """
         # Unsigned byte
         if form == 1:
